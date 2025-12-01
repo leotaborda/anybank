7d330225b2274434fb1a3c6106223104e7dd781b
oei7ca <leonardo.taborda@br.bosch.com>
2025-12-01 11:13:07 -0300

    feat: created TransactionModel

---
+++ src/app/models/transaction.ts
@@ -0,0 +1,11 @@
+export class Transaction {
+    constructor (
+        public readonly type: TypeTransaction,
+        public readonly value: number
+    ) {}
+}
+
+export enum TypeTransaction {
+    DEPOSIT = 'deposit',
+    WITHDRAWAL = 'withdrawal'
+}
