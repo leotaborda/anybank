42a38e40882c3e7835017d2f868bb0219fadb4e5
oei7ca <leonardo.taborda@br.bosch.com>
2025-12-01 15:34:13 -0300

    feat: component created to list transaction statement

---
+++ src/app/components/statement/transaction/transaction.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { Transaction, TypeTransaction } from '../../../models/transaction';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-transaction',
+  imports: [CurrencyPipe, DatePipe],
+  templateUrl: './transaction.component.html',
+  styleUrl: './transaction.component.css',
+})
+export class TransactionComponent {
+  transaction = input.required<Transaction>();
+  TransactionType = TypeTransaction;
+}
