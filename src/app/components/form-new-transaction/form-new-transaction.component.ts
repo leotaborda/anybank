aadaece67e394bfcc358cc81c2674378a8d1a300
oei7ca <leonardo.taborda@br.bosch.com>
2025-12-01 13:53:28 -0300

    feat: added TitleCasePipe

--- src/app/components/form-new-transaction/form-new-transaction.component.ts
+++ src/app/components/form-new-transaction/form-new-transaction.component.ts
@@ -1,18 +1,19 @@
 import { Component, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Transaction, TypeTransaction } from '../../models/transaction';
+import { KeyValuePipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-form-new-transaction',
-  imports: [FormsModule],
+  imports: [FormsModule, KeyValuePipe, TitleCasePipe],
   templateUrl: './form-new-transaction.component.html',
   styleUrl: './form-new-transaction.component.css',
 })
 export class FormNewTransactionComponent {
   transactionType = "";
   transactionValue = "";
-
   createdTransaction = output<Transaction>();
+  TransactionTypeEnum = TypeTransaction;
 
   onSubmit() {
     const transaction = new Transaction(
