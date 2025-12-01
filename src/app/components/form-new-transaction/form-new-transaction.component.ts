ba60baa0d3422105cfd02a7ef2bd20803cdcc10f
oei7ca <leonardo.taborda@br.bosch.com>
2025-12-01 10:44:43 -0300

    feat: create FormNewTransactionComponent

---
+++ src/app/components/form-new-transaction/form-new-transaction.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-new-transaction',
+  imports: [FormsModule],
+  templateUrl: './form-new-transaction.component.html',
+  styleUrl: './form-new-transaction.component.css',
+})
+export class FormNewTransactionComponent {
+  transactionType = "";
+  transactionValue = "";
+
+  onSubmit() {
+    console.log(this.transactionType);
+    console.log(this.transactionValue);
+  }
+}
